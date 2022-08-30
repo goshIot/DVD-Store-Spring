baf609f84590b14d04432a1acb72bfe4fded6f5f
goshACA <kunjiyan.gohar@gmail.com>
2022-08-30 14:23:59 +0200

    task-3: add abstraction layer

--- src/main/java/com/mycompany/dvdstore/controller/MovieController.java
+++ src/main/java/com/mycompany/dvdstore/controller/MovieController.java
@@ -2,12 +2,13 @@ package com.mycompany.dvdstore.controller;
 
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.service.MovieService;
+import com.mycompany.dvdstore.service.MovieServiceInterface;
 
 import java.util.Scanner;
 
 public class MovieController {
 
-    private MovieService movieService = new MovieService();
+    private MovieServiceInterface movieService;
 
 
     public void addUsingConsole(){
--- src/main/java/com/mycompany/dvdstore/service/MovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieService.java
@@ -3,16 +3,15 @@ package com.mycompany.dvdstore.service;
 import com.mycompany.dvdstore.entity.Movie;
 import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
 import com.mycompany.dvdstore.repository.MovieRepository;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 
 public class MovieService {
 
-    private MovieRepository movieRepository = new MovieRepository();
-
-    private GoLiveMovieRepository goLiveMovieRepository = new GoLiveMovieRepository();
+    private MovieRepositoryInterface movieRepository ;
 
     public void registerMovie(Movie movie){
 
-        goLiveMovieRepository.add(movie);
+        movieRepository.add(movie);
     }
 
 
