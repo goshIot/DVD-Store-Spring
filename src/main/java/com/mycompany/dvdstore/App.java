9a600a3727e3071eed4202e7b128679234a8d907
goshACA <kunjiyan.gohar@gmail.com>
2022-08-30 15:54:55 +0200

    task-6: add applicationContext

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,16 +3,15 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.repository.FileMovieRepository;
 import com.mycompany.dvdstore.service.DefaultMovieService;
+import com.mycompany.dvdstore.service.MovieServiceInterface;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App
 {
     public static void main( String[] args )
     {
-        DefaultMovieService defaultMovieService = new DefaultMovieService();
-        defaultMovieService.setMovieRepository(new FileMovieRepository());
-
-        MovieController movieController = new MovieController();
-        movieController.setMovieService(defaultMovieService);
-        movieController.addUsingConsole();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        applicationContext.getBean(MovieController.class).addUsingConsole();
     }
 }
