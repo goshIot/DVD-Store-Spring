ca6c6b862aca69b80af0158492b78196cac6a072
goshACA <kunjiyan.gohar@gmail.com>
2022-09-01 01:33:50 +0200

    task-12: replace xml usage with a java class

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,13 +2,14 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         applicationContext.getBean(MovieController.class).addUsingConsole();
     }
 }
