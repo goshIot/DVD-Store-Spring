dcd0ab8f3fe31bd021cf7058de1b086d4c9a77ee
goshACA <kunjiyan.gohar@gmail.com>
2022-09-01 01:42:46 +0200

    task-13: delete AppConfig

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,13 +3,20 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller", "com.mycompany.dvdstore.service", "com.mycompany.dvdstore.repository.file"} )
+@PropertySource("application.properties")
+//for complementary xml
+//@ImportResource("clas
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
         applicationContext.getBean(MovieController.class).addUsingConsole();
     }
 }
