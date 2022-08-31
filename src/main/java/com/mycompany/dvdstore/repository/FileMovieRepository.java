ff41a313a7b79c85ee5759a871e6a11b18da9b94
goshACA <kunjiyan.gohar@gmail.com>
2022-09-01 00:28:16 +0200

    task-7-8: add autowiring byType

--- src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -2,15 +2,22 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileMovieRepository implements MovieRepositoryInterface {
 
+    public void setFile(String str) {
+        this.file = new File(str);
+    }
+
+    private File file;
+
     public void add(Movie movie){
         FileWriter writer;
         try{
-            writer=new FileWriter("movies.txt",true);
+            writer=new FileWriter(file,true);
             writer.write(movie.getTitle() + ";" + movie.getGenre() + "\n");
             writer.close();
         }
