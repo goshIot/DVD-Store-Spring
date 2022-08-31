75b4882cce504c5e2eaff3a4b734360a6209f560
goshACA <kunjiyan.gohar@gmail.com>
2022-09-01 01:17:09 +0200

    task-11: add component scan and downgrade jdk version

--- src/main/java/com/mycompany/dvdstore/service/MovieServiceInterface.java
+++ src/main/java/com/mycompany/dvdstore/service/MovieServiceInterface.java
@@ -1,6 +1,7 @@
 package com.mycompany.dvdstore.service;
 
 import com.mycompany.dvdstore.entity.Movie;
+import org.springframework.stereotype.Service;
 
 public interface MovieServiceInterface {
     void registerMovie(Movie movie);
