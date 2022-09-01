f6895c0051bfa302090cb291f594481f4aa6b64b
goshACA <kunjiyan.gohar@gmail.com>
2022-09-01 16:50:22 +0200

    task-15: add Apache Tomcat

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -4,19 +4,13 @@ import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-//for complementary xml
-//@ImportResource("clas
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args)
     {
-        ApplicationContext applicationContext =  SpringApplication.run(App.class);
+        ApplicationContext applicationContext =  SpringApplication.run(App.class, args);
         applicationContext.getBean(MovieController.class).addUsingConsole();
     }
 }
