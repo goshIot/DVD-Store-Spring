c21691ea3aa65fe947fe0561c7488b79020e32ce
goshACA <kunjiyan.gohar@gmail.com>
2022-09-01 02:07:56 +0200

    task-14: add SpringBootApplication

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,22 +1,22 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller", "com.mycompany.dvdstore.service", "com.mycompany.dvdstore.repository.file"} )
-@PropertySource("application.properties")
+@SpringBootApplication
 //for complementary xml
 //@ImportResource("clas
 public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext applicationContext =  SpringApplication.run(App.class);
         applicationContext.getBean(MovieController.class).addUsingConsole();
     }
 }
